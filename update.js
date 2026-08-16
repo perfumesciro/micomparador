8ebb83a7ae9c0fb6750c9d9673d962e88f86223a
perfumesciro <matifreue@gmail.com>
2026-08-16 00:24:56 -0300

    Update update.js

--- update.js
+++ update.js
@@ -1,14 +1,1 @@
-const fs = require("fs");
 
-const data = {
-  pescado: [
-    { super: "Carrefour", precio: Math.floor(Math.random() * 3000) },
-    { super: "Jumbo", precio: Math.floor(Math.random() * 3000) },
-    { super: "Makro", precio: Math.floor(Math.random() * 3000) },
-    { super: "Diarco", precio: Math.floor(Math.random() * 3000) }
-  ]
-};
-
-fs.writeFileSync("data.json", JSON.stringify(data, null, 2));
-
-console.log("OK actualizado");
